81e839a1d205453a274ac92697edfc0e9295d56f
Nhuan Do <donhuanvn96@gmail.com>
2022-07-04 00:04:21 +0700

    done the exercise of the third lesson

--- exercise-complex-type/src/main.rs
+++ exercise-complex-type/src/main.rs
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-use std::collections::HashMap;
-use itertools::Itertools;
-
-struct School {
-    students: HashMap<String, u32>
-=======
-fn main() {
-    println!("Hello, world!");
-}
-
-
 // Bài tập
 // Cho ngữ cảnh như sau : Một ngôi trường cần lập danh
 //sách thông tin sinh viên bao gồm tên sinh viên và điểm của sinh viên đó.
 // với mục đích thống kê kiểm tra giáo dục của ngôi trường này
 
-
 /*-----------------------------*/
 // Gợi ý:
-// Định nghĩa bằng struct, mọi người nên sử dụng HashMap 
+// Định nghĩa bằng struct, mọi người nên sử dụng HashMap
 // Tại sao lại sử dụng HashMap và ko phải Vec
 //https://doc.rust-lang.org/std/collections/struct.HashMap.html
 // struct School {
@@ -26,7 +13,7 @@ fn main() {
 // }
 
 // trong trường hợp này thì String : tên của sinh viên đó
-// u32 là điểm số 
+// u32 là điểm số
 
 // Một số yêu cầu như sau:
 
@@ -49,12 +36,11 @@ fn main() {
 // ví dụ: hiện tại danh sách gồm có (Alice, 3), (Bob, 10), (Charlie,3)
 // liệt kê danh sách học sinh có cùng 3 điểm : [Alice, Charlie]
 
-// Yêu cầu trả về: danh sách sinh viên là alphabet theo tên 
+// Yêu cầu trả về: danh sách sinh viên là alphabet theo tên
 
-// Gợi ý: 
+// Gợi ý:
 // ví dụ : Ban đầu [(Alice, 10), (Bob,2), (Eve,4), (Long,2)] -> [(Bob, 2), (Long,2), (Eve,4), (Alice,10)]
 //định nghĩa hàm "find_student" có tham số là điểm số -> trả về danh sách các sinh viên có cùng điểm số mong muốn
-
 
 // Các bạn phải vuợt qua một số test case như sau :
 
@@ -76,24 +62,21 @@ fn main() {
 // với điểm số 4 thì ta có sinh viên nào: -> [Alice, Bob] not [Bob ,Alice]
 // vì cần tên theo alphabet
 
-
 /*-----------------------------*/
-// Nếu mọi người làm xong rùi thì có thể làm advance hơn bằng cách 
+// Nếu mọi người làm xong rùi thì có thể làm advance hơn bằng cách
 // sử dụng Generic type cho điểm số không nhất thiết phải U32 nữa mà có thể "A+", "B+" chẳng hạn (string)
 /*-----------------------------*/
 
-// Sườn thông tin cho mọi người dễ làm
+// BASIC ==============================================================================
+/* use std::collections::HashMap;
+use itertools::Itertools;
 
-
-
-pub struct School {
-    // !TODO
->>>>>>> eef58d8133729363ea926550dfd31fdc7bb82bf7
+struct School {
+    students: HashMap<String, u32>
 }
 
 impl School {
     pub fn new() -> School {
-<<<<<<< HEAD
         Self { students: HashMap::new() }
     }
     pub fn add(&mut self, student: &str, grade: u32) {
@@ -119,13 +102,9 @@ impl School {
     }
 }
 
-// fn print_type_of<T>(_: &T) {
-//     println!("{}", std::any::type_name::<T>())
-// }
-
 fn main() {
     let mut school = School::new();
-    // Testcase 1: Khoi tao danh sach rong
+
     println!("\n\nTestcase 1: Creating an empty school (vector)");
     school.print_students();
 
@@ -133,7 +112,7 @@ fn main() {
     school.add("Lee", 2);
     school.add("Nancy", 3);
     school.print_students();
-    
+
     println!("\n\nTestcase 3: Filtering students with a specified grade");
     school.add("Bob", 4);
     school.add("Alice", 4);
@@ -141,37 +120,73 @@ fn main() {
     let students_with_grade_4 = school.grade(4);
     println!("Students with grade of 4: {:?}", students_with_grade_4);
 }
-=======
-        unimplemented!()
+ */
 
+// ADVANCED ==============================================================================
+use core::fmt::Debug;
+use itertools::Itertools;
+use std::cmp::Eq;
+use std::collections::HashMap;
+use std::hash::Hash;
+
+struct School<T> {
+    students: HashMap<String, T>,
+}
+
+impl<T: Debug + Hash + Eq + Ord + Clone> School<T> {
+    pub fn new() -> School<T> {
+        Self {
+            students: HashMap::new(),
+        }
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!()
-
-        
+    pub fn add(&mut self, student: &str, grade: T) {
+        self.students.entry(student.to_string()).or_insert(grade);
     }
 
-    pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
-
+    pub fn print_students(&self) {
+        let count = self.students.len();
+        println!("There is {count} students.");
+        for (student, grade) in &self.students {
+            println!("{}: '{:?}'", student, grade);
+        }
     }
 
+    // find distinct grades (for TEST CASE 2)
+    pub fn grades(&self) -> Vec<&T> {
+        // get values
+        // get distinct values
+        // sort with ascending order
+        self.students.values().unique().sorted().collect_vec()
+    }
 
-    pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!()
-
-        
+    pub fn grade(&self, grade: T) -> Vec<String> {
+        // clone for mutating after.
+        // filter students with the specified grade.
+        // sort with ascending order of student's name.
+        let mut students = self.students.clone();
+        students.retain(|_, v| *v == grade);
+        students.into_keys().sorted().collect_vec()
     }
 }
 
+fn main() {
+    println!("\n\nTestcase 1: Creating an empty school (vector)");
+    let mut school = School::new();
+    school.print_students();
 
+    println!(
+        "\n\nTestcase 2: Adding new students into the school, finding & sorting distinct grades"
+    );
+    school.add("Nancy", 3);
+    school.add("Lee", 2);
+    school.add("Lee2", 2);
+    println!("Distinct grades: {:?}", school.grades());
 
-
-
-
-
-
-
-
->>>>>>> eef58d8133729363ea926550dfd31fdc7bb82bf7
+    println!("\n\nTestcase 3: Filtering students with a specified grade & sorting name in alphabet order.");
+    school.add("Tom", 4);
+    school.add("Bob", 4);
+    school.add("Alice", 4);
+    let students_with_grade_4 = school.grade(4);
+    println!("Students with grade of 4: {:?}\n\n", students_with_grade_4);
+}
